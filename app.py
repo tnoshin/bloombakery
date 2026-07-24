c6a833fbff232fe07d7a9be2ecd136b2a671214b
unknown <tnoshin>
2026-07-24 20:14:34 +0600

    switch to postgres for persistent storage

--- app.py
+++ app.py
@@ -15,7 +15,10 @@ app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY')
 api_key = os.getenv('GEMINI_API_KEY')
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
+database_url = os.getenv('DATABASE_URL', 'sqlite:///chat.db')
+if database_url.startswith('postgres://'):
+    database_url = database_url.replace('postgres://','postgresql://', 1)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url
 
 db= SQLAlchemy(app)
 
